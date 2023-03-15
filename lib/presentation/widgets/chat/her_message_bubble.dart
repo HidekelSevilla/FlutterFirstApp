adc3a3074b258ed926abead946d7bbc2ffb90982
Hidekel Sevilla <hidekel.sevilla.morlet@gmail.com>
2023-03-15 01:04:28 -0600

    Functions added yet final version?

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble({super.key});
+  final Message message;
+  const HerMessageBubble({super.key, required this.message});
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +14,18 @@ class HerMessageBubble extends StatelessWidget {
         Container(
           decoration: BoxDecoration(
               color: colors.secondary, borderRadius: BorderRadius.circular(20)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'Hola Mundop',
-              style: TextStyle(color: Colors.white),
+              message.text,
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
         const SizedBox(height: 5),
 
         //todo Imagen
-        _ImageBubble(),
+        _ImageBubble(imageUrl: message.imageUrl!),
 
         const SizedBox(
           height: 10,
@@ -34,24 +36,27 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+  final String imageUrl;
+
+  const _ImageBubble({ required this.imageUrl});
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: Image.network(
-          'https://yesno.wtf/assets/no/4-122be48db47678331dbef3dd6ac6ff56.gif',
+          imageUrl,
           width: size.width * 0.7,
           height: 180,
           fit: BoxFit.cover,
           loadingBuilder: (context, child, loadingProgress) {
             if (loadingProgress == null) return child;
             return Container(
-              width: size.width * 0.7,
-              height: 180,
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: const Text('Mi Amor<3 esta enviando una imagen...')
-            );
+                width: size.width * 0.7,
+                height: 180,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: const Text('Mi Amor<3 esta enviando una imagen...'));
           },
         ));
   }
--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:provider/provider.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+  final ValueChanged<String> onValue;
+  const MessageFieldBox({super.key, required this.onValue});
 
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
     // final colors = Theme.of(context).colorScheme;
 
     final textController = TextEditingController();
@@ -14,30 +18,44 @@ class MessageFieldBox extends StatelessWidget {
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineImputBorder,
       focusedBorder: outlineImputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
+          if ((textController.value.text.trim()).isEmpty) return;
           final textValue = textController.value.text; //Toma el valor del texto
-          print('TextValue: $textValue');
           textController.clear();
+          onValue(textValue.trim());
         },
       ),
     );
 
-    return TextFormField(
-     //Comportamiento de Texto form en aplicacion
-      onTapOutside: (event) {
-        focusNode.unfocus();
-      },
-      focusNode: focusNode,
-      controller: textController,
-      decoration: inputDecoration,
-      onFieldSubmitted: (value) {
-        textController.clear();
-        focusNode.requestFocus();
-      });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 3),
+      child: TextFormField(
+        
+          //Comportamiento de Texto form en aplicacion
+
+          onTapOutside: (event) {
+            focusNode.unfocus();
+          },
+          focusNode: focusNode,
+          controller: textController,
+          decoration: inputDecoration,
+          onChanged:(value) => chatProvider.moveScrollToBottom() ,
+          onTap: () => chatProvider.moveScrollToBottom(),
+          onFieldSubmitted: (value) {
+            if ((value.trim()).isEmpty) {
+              focusNode.requestFocus();
+              return;
+            }
+            onValue(value.trim());
+            textController.clear();
+            focusNode.requestFocus();
+          }),
+    );
   }
 }
